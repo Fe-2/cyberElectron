c4d764d90a9e1e09a0277974893ae0463d4bc2d7
yzr <yzr.main@outlook.com>
2021-04-03 13:56:40 +0800

    all super

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>{{a.b}}</h1>
+</template>
+
+<script>
+import {
+  reactive,
+  ref,
+  toRef,
+ unref,
+ isProxy,
+  } from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const count = reactive({
+      a: {
+        b:1
+      }
+    });
+    const s = toRef(count, "a");
+    console.log(s.value);
+    setTimeout(()=>{
+      count.a.b = 3232;
+    },2000);
+    return {
+      ...count
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin: 60px;
+}
+</style>
